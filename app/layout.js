1c0ad5ecbe00402a323f40e7bf550f8411282a26
nilay06 <nilay06jain@gmail.com>
2024-09-30 23:00:21 +0530

    Navbar done and configured done

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,20 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import AppWalletProvider from "@/components/AppWalletProvider";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "feeflex",
   description: "",
 };
 
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className="font-unbounded">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem={true}>
           <AppWalletProvider>
             <Navbar />
